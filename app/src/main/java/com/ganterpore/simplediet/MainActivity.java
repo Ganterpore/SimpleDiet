bc7a8ed9b7ed4669eb34daf8db9554acf21335e6
mhatfield@student.unimelb.edu.au <j95Z4@bz0fQ>
2019-10-30 19:02:59 +1100

    project opened

---
+++ app/src/main/java/com/ganterpore/simplediet/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ganterpore.simplediet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
